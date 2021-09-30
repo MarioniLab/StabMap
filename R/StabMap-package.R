74640a78f7090f0c218cdec5baae0f9ab13a73ad
shazanfar <shazanfar@gmail.com>
2021-09-30 18:12:37 +0100

    add first function

---
+++ R/StabMap-package.R
@@ -0,0 +1,13 @@
+#' StabMap
+#'
+#' Mosaic single cell data integration using non-overlapping features
+#'
+#'
+#' @docType package
+#'
+#' @author Shila Ghazanfar \email{shazanfar@gmail.com}
+#'
+#' @name StabMap
+#'
+#' @import scran
+NULL
---
+++ R/reWeightEmbedding.R
@@ -0,0 +1,42 @@
+#' Re-weight embedding
+#'
+#' Re-weights embedding according to given weights
+#'
+#' @param embedding Output from stabMap
+#' @param weights weights vector
+#' @param factor numeric value
+#'
+#' @return matrix
+#'
+#' @examples
+#'
+#' @export
+reWeightEmbedding = function(embedding, weights = NULL, factor = 1e6) {
+
+  # embedding is a cells x dimensions matrix
+  # weights is an optional named list (names correspond to all before the underscore)
+  # for weighting each embedding component
+  # factor is a multiplicative value to avoid tiny numbers
+  cols = as.character(interaction(gsub("_.+", "", colnames(embedding)),
+                                  ifelse(grepl("_PC", colnames(embedding)), "PC", "LD"), sep = "_"))
+
+  cols_split = split(colnames(embedding), cols)
+
+  if (is.null(weights)) {
+    weights = lapply(cols_split, function(x) 1)
+  }
+
+  norms = lapply(cols_split, function(cols) {
+    sum(abs(embedding[,cols]))
+  })
+
+  norms_long = unsplit(norms, cols)
+  weights_long = factor*unlist(weights)[cols]
+
+  embedding_norm = t( (t(embedding) / norms_long) * weights_long)
+  if (FALSE) {
+    barplot(colSums(embedding_norm^2))
+    boxplot(embedding_norm)
+  }
+  return(embedding_norm)
+}
